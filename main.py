3efa4f61c9c1239170af90af747cf4597562b90d
jjulia01 <153107471+jjulia01@users.noreply.github.com>
2024-11-04 14:11:12 +0300

    Задача 2024.10.23.04
    Сохраните из этой презентации только слова, всякие "Правила", "Тур 1" и другие объяснение не нужны.
    
    Сохраните все слова в файл words.txt
    Закомитьте его тоже.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os#подключаем библиотеку os
 from pptx import Presentation#ипортируем и подключаем из библиотеки pptx класс Presentation
 FILE_NAME='Osennyaya_igra_3.pptx'#записываем в переменную FILE_NAME путь к файлу 'Osennyaya_igra_3.pptx'
+words = list()
 
 current_file=os.path.realpath(__file__)#в переменную записываем путь к текущему файлу
 print(f'{current_file=}')#выводим путь текущего файла
@@ -12,4 +13,10 @@ for slide in prs.slides:#перебираем слайды
     for shape in slide.shapes:#перебираем форму
         if not shape.has_text_frame:#условие, чтобы на слайде не было текста
             continue#если условие выполняется, продолжаем перебирать слайды
-        print(shape.text)#если условие не выполняется, то выводим текст со слайда 
+        if " " in shape.text or ":" in shape.text or "-" in shape.text or "СУПЕРКРОКО" in shape.text:
+            continue
+        words.append(shape.text)#если условие не выполняется, то выводим текст со слайда 
+
+
+with open("words.txt", "w", encoding="utf-8") as file:
+    print(*words, file=file)
