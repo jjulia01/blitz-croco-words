18f10ad87aa8a7e7a70d4724fffe56701b296f68
jjulia01 <153107471+jjulia01@users.noreply.github.com>
2024-11-04 10:59:00 +0300

    Задача 2024.10.23.06
    Разархивируйте архив и достаньте один файл презентации.
    
    Откройте файл презентации в программе с помощью объекта Presentation библиотеки pptx

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from pptx import Presentation
 current_file=os.path.realpath(__file__)
 current_directory=os.path.dirname(current_file)
-print(current_file)
+print(current_file) 
+prs = Presentation('Osennyaya_igra_3.pptx')
