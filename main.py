d4de6f6575010a1474bf8e2198680e0552278151
jjulia01 <153107471+jjulia01@users.noreply.github.com>
2024-11-04 13:22:19 +0300

    Задача 2024.10.25.02
    Прокомментируйте каждую строку вашего кода из задачи 2024.10.23.07. переделано

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os#подключаем библиотеку os
 from pptx import Presentation#ипортируем и подключаем из библиотеки pptx класс Presentation
-FILE_NAME='Osennyaya_igra_3.pptx'#объявляем и инициализируем переменную FILE_NAME файлом 'Osennyaya_igra_3.pptx'
+FILE_NAME='Osennyaya_igra_3.pptx'#записываем в переменную FILE_NAME путь к файлу 'Osennyaya_igra_3.pptx'
 
-current_file=os.path.realpath(__file__)#объявляем и инициализируем переменную current_file результатом функции os.path.realpath(__file__)
-current_directory=os.path.dirname(current_file)#объявляем и инициализируем переменную current_directory результатом функции os.path.dirname(current_file)
-print(current_file) #выводим значение переменной current_file
-prs = Presentation('Osennyaya_igra_3.pptx')#открфваем файл Osennyaya_igra_3.pptx' и переносим данные в переменную prs
+current_file=os.path.realpath(__file__)#в переменную записываем путь к текущему файлу
+print(f'{current_file=}')#выводим путь текущего файла
+current_directory=os.path.dirname(current_file)#в переменную записываем путь к текущей директории
+print(f'{current_directory=}')#выводим путь текущей директории
 
-prs = Presentation(FILE_NAME)#равнозначно команде prs = Presentation('Osennyaya_igra_3.pptx')
-for slide in prs.slides:#запускаем цикл for prs.slides по slide
-    for shape in slide.shapes:#запускаем цикл for slide.shapes по shape
-        if not shape.has_text_frame:#условие выполняется не shape.has_text_frame
-            continue#продолжить
-        print(shape.text_frame)#выводим результат shape.text_frame
+prs = Presentation(FILE_NAME)#записываем в переменную данные из файла
+for slide in prs.slides:#перебираем слайды
+    for shape in slide.shapes:#перебираем форму
+        if not shape.has_text_frame:#условие, чтобы на слайде не было текста
+            continue#если условие выполняется, продолжаем перебирать слайды
+        print(shape)#если условие не выполняется, то выводим текст со слайда 
