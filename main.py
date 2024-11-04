12b1608b46c08caa0442f6df1a2b29d91ee12236
jjulia01 <153107471+jjulia01@users.noreply.github.com>
2024-11-04 12:29:32 +0300

    Задача 2024.10.25.02
    Прокомментируйте каждую строку вашего кода из задачи 2024.10.23.07.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import os
-from pptx import Presentation
-current_file=os.path.realpath(__file__)
-current_directory=os.path.dirname(current_file)
-print(current_file) 
-prs = Presentation('Osennyaya_igra_3.pptx')
+import os#подключаем библиотеку os
+from pptx import Presentation#ипортируем и подключаем из библиотеки pptx класс Presentation
+FILE_NAME='Osennyaya_igra_3.pptx'#объявляем и инициализируем переменную FILE_NAME файлом 'Osennyaya_igra_3.pptx'
+
+current_file=os.path.realpath(__file__)#объявляем и инициализируем переменную current_file результатом функции os.path.realpath(__file__)
+current_directory=os.path.dirname(current_file)#объявляем и инициализируем переменную current_directory результатом функции os.path.dirname(current_file)
+print(current_file) #выводим значение переменной current_file
+prs = Presentation('Osennyaya_igra_3.pptx')#открфваем файл Osennyaya_igra_3.pptx' и переносим данные в переменную prs
+
+prs = Presentation(FILE_NAME)#равнозначно команде prs = Presentation('Osennyaya_igra_3.pptx')
+for slide in prs.slides:#запускаем цикл for prs.slides по slide
+    for shape in slide.shapes:#запускаем цикл for slide.shapes по shape
+        if not shape.has_text_frame:#условие выполняется не shape.has_text_frame
+            continue#продолжить
+        print(shape.text_frame)#выводим результат shape.text_frame
